0d5d7f59978b06092b5387c2db8696442fb0aff2
Maxim Shikunov <schikk@ukr.net>
2019-02-05 13:01:18 +0200

    Clone project. Add empty floor category. Test commit

---
+++ category_floors.php
@@ -0,0 +1,3 @@
+<?php require('templates/common/header.php') ?>
+<?php require('templates/category-floors.php') ?>
+<?php require('templates/common/footer.php') ?>
---
+++ templates/category-floors.php
@@ -0,0 +1,5 @@
+<div class="page-wrapper-category-floors">
+	<div class="category-floors-filter-container">
+		<a href="page_genplan.php" class="single-app-back animation-element-default in-view">повернутись до генплану</a>
+	</div>
+</div>
